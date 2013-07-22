8682802afb67801a2463aa120d96d71f732381a2
Arunkumar <arunram.krish@gmail.com>
2013-07-22 09:36:11 +0530

    Initial build converted to a maven project build and uploaded

---
+++ paataprot/src/main/java/com/ramselabs/paataprot/bean/Managed.java
@@ -0,0 +1,19 @@
+package com.ramselabs.paataprot.bean;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name="my Bean")
+@SessionScoped
+public class Managed {
+
+private String pageToDisplay = "home.xhtml";
+
+  public String getPageToDisplay(){
+      return this.pageToDisplay;
+  }
+
+  public void setPageToDisplay(String pageToDisplay){
+     this.pageToDisplay = pageToDisplay;
+  }
+}
